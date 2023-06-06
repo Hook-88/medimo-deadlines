bcab7934eac731fbcc732bc523de347a351519e1
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2023-06-06 13:11:31 -0500

    Cleanup code

--- script.js
+++ script.js
@@ -1,52 +1,29 @@
-import {Deadlines} from './deadline.js'
+import { Deadlines } from './deadline.js'
 import { clearContainer, resetTimeInput, resetDayInput } from './helpers.js'
 
 let newDeadline = new Deadlines
 
 document.addEventListener('click', e => { 
-  if (e.target.id === 'calc-deadline-today-btn') {
-    renderDeadlineToday()
-  } else if (e.target.id === 'calc-deadline-baxter-btn') {
-      newDeadline.setDeadlineBaxter()
-      if(newDeadline.deadlineBaxter) {
-        renderBaxterDeadline()
-      }  
-  } else if (e.target.id === 'calc-deadline-delivery-btn') {
-    newDeadline.setDeadlineDelivery()
-    if (newDeadline.deadlineDelivery) {
-      renderDeliveryDeadline()
-    }
-  } else {
-    if (e.target.dataset.reset) {
-      clearContainer(e.target.dataset.reset)
-      resetTimeInput(document.querySelectorAll(`[data-time-input="${e.target.dataset.reset}"]`))
-      resetDayInput(document.querySelectorAll(`[data-day-input="${e.target.dataset.reset}"]`))
-    } else if(e.target.dataset.copy) {
-      navigator.clipboard.writeText(newDeadline[e.target.dataset.copy])
-    }
+  if (e.target.dataset.reset) {
+    clearContainer(e.target.dataset.reset)
+    resetTimeInput(document.querySelectorAll(`[data-time-input="${e.target.dataset.reset}"]`))
+    resetDayInput(document.querySelectorAll(`[data-day-input="${e.target.dataset.reset}"]`))
+  } else if(e.target.dataset.copy) {
+    navigator.clipboard.writeText(newDeadline[e.target.dataset.copy])
   }
-  
 })
 
 document.addEventListener('change', e => {
-
   if(e.target.id === 'deadline-today-time') {
     renderDeadlineToday()
   } else if(e.target.dataset.baxter) {
     newDeadline.setDeadlineBaxter()
-    if(newDeadline.deadlineBaxter) {
-      renderBaxterDeadline()
-    }
+    newDeadline.deadlineBaxter ? renderBaxterDeadline() : false
   } else if(e.target.dataset.delivery) {
     newDeadline.setDeadlineDelivery()
-    if (newDeadline.deadlineDelivery) {
-      renderDeliveryDeadline()
-    }
+    newDeadline.deadlineDelivery ? renderDeliveryDeadline() : false
   }
-    
-
 })
-
 
 function renderDeadlineToday() {
   document.getElementById('deadline-today-result').innerHTML = newDeadline.getDeadlineTodayHtml()
@@ -59,6 +36,7 @@ function renderBaxterDeadline() {
 function renderDeliveryDeadline() {
   document.getElementById('deadline-delivery-result').innerHTML = newDeadline.getDeadlineDeliveryHtml()
 }
+
 
 
 
