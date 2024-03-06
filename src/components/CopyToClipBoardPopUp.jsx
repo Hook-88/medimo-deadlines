156f985e055b36e0da56364108fc1734212c806e
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-03-06 18:59:52 -0500

    Subject line (try to keep under 50 characters)
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- src/components/CopyToClipBoardPopUp.jsx
+++ src/components/CopyToClipBoardPopUp.jsx
@@ -3,7 +3,7 @@ import Card from "./Card/Card"
 export default function CopyToClipboardPopUp() {
     
     return (
-        <Card className="border border-green-800 bg-green-800 fixed inset-x-1/3 top-10">
+        <Card className="bg-green-800 fixed inset-x-1/3 top-10">
             <h1>Deadline copied</h1>
         </Card>
     )
